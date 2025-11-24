3367ade804fbb162afbd5549faa40bfd6e3cba1b
Ian Mogalo <mogaloian05@gmail.com>
2025-11-24 12:58:42 +0300

    Changed The Database Name - movieratings_db

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/moviesproject";
+    private static final String URL = "jdbc:mysql://localhost:3306/movieratings_db";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
